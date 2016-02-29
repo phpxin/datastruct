9db1729cc63906870827ea632198546f55d5f886
llxx <lixin65535@126.com>
2016-02-29 19:44:09 +0800

    #

--- linklist/linklist.h
+++ linklist/linklist.h
@@ -8,6 +8,7 @@
 #ifndef LINKLIST_H_
 #define LINKLIST_H_
 
+/* 支持自定义类型 */
 #ifndef UDF_ELETYPE
 typedef int EleType ;
 #endif
@@ -24,27 +25,27 @@ typedef struct _LLink{
 	int len;
 } LLink;
 
-//初始化链表
-//返回：1 成功 0 失败
+/* 初始化链表
+返回：1 成功 0 失败 */
 int init_llink(LLink *llink) ;
 
-//链表成员数
-//返回：int 成员数量
+/*链表成员数
+返回：int 成员数量 */
 int len_llink(LLink *llink) ;
 
-//是否为空表
-//返回：1 是 0 否
+/*是否为空表
+返回：1 是 0 否 */
 int isempty_llink(LLink *llink) ;
 
-//添加数据到链表
-//返回：1 成功 0 失败
+/*添加数据到链表
+返回：1 成功 0 失败 */
 int append_data_llink(LLink *llink, EleType e) ;
 
-//从链表中删除数据
-//返回：int 删除成员数
+/*从链表中删除数据
+返回：int 删除成员数 */
 int del_data_llink(LLink *llink, EleType e, int (*equals_callback_llink)(EleType , EleType )) ;
 
-//遍历链表
+/*遍历链表 */
 void foreach_llink(LLink *llink, void (*iterator_callback_llink)(EleType)) ;
 
 #endif /* LINKLIST_H_ */
